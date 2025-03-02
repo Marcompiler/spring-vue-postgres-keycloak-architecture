8635c5dba040f7a6613d4f2063a60ee55f6c10b9
Marcompiler <marcompiler@outlook.com>
2025-03-02 12:37:08 +0100

    Ajout du projet de base Spring avec "project file"

---
+++ backend/app/src/main/java/com/marcompiler/test_architecture_rest_jpa_oauth2/TestArchitectureRestJpaOauth2Application.java
@@ -0,0 +1,13 @@
+package com.marcompiler.test_architecture_rest_jpa_oauth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestArchitectureRestJpaOauth2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestArchitectureRestJpaOauth2Application.class, args);
+	}
+
+}
---
+++ backend/app/src/test/java/com/marcompiler/test_architecture_rest_jpa_oauth2/TestArchitectureRestJpaOauth2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcompiler.test_architecture_rest_jpa_oauth2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestArchitectureRestJpaOauth2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
