3d93a9ae22f6b7fefb255921634867caf04b1f59
Marcompiler <marcompiler@outlook.com>
2025-04-13 18:25:39 +0200

    interfaces renamed

--- backend/app/src/main/java/com/marcompiler/test_architecture_rest_jpa_oauth2/common/IIdentifiableEntity.java
+++ backend/app/src/main/java/com/marcompiler/test_architecture_rest_jpa_oauth2/common/IIdentifiableEntity.java
@@ -4,7 +4,7 @@ package com.marcompiler.test_architecture_rest_jpa_oauth2.common;
  * Defines the use of an identifier of a given type.
  * @param <T> The data type of the identifier.
  */
-public interface IdentifiableEntity<T> {
+public interface IIdentifiableEntity<T> {
     T getId();
 
     void setId(T id);
--- backend/app/src/main/java/com/marcompiler/test_architecture_rest_jpa_oauth2/common/IVersionedEntity.java
+++ backend/app/src/main/java/com/marcompiler/test_architecture_rest_jpa_oauth2/common/IVersionedEntity.java
@@ -3,6 +3,6 @@ package com.marcompiler.test_architecture_rest_jpa_oauth2.common;
 /**
  * Defines a versioning for entities.
  */
-public interface VersionedEntity {
+public interface IVersionedEntity {
     Integer getVersion();
 }
