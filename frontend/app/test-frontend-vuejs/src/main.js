5484373177f8c40c7c699dfae4f43c9a523ca77a
Marcompiler <marcompiler@outlook.com>
2025-04-19 20:59:09 +0200

    Frontend : Keycloak auth -> OK

--- frontend/app/test-frontend-vuejs/src/main.js
+++ frontend/app/test-frontend-vuejs/src/main.js
@@ -3,9 +3,29 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import keycloak from './keycloak'
 
-const app = createApp(App)
+await keycloak
+  .init({
+    onLoad: 'login-required',
+  })
+  .then((authenticated) => {
+    if (authenticated) {
+      console.log('✅ Authenticated with Keycloak')
 
-app.use(router)
+      // Auto-refresh token
+      setInterval(() => {
+        keycloak.updateToken(60).catch(() => keycloak.logout())
+      }, 6000)
 
-app.mount('#app')
+      const app = createApp(App)
+
+      // Ajouter keycloak globalement
+      app.config.globalProperties.$keycloak = keycloak
+
+      app.use(router)
+      app.mount('#app')
+    } else {
+      console.error('❌ Authentication failed')
+    }
+  })
--- frontend/app/test-frontend-vuejs/src/views/HomeView.vue
+++ frontend/app/test-frontend-vuejs/src/views/HomeView.vue
@@ -1,9 +1,15 @@
 <script setup>
 import TheWelcome from '../components/TheWelcome.vue'
+import keycloak from '../keycloak'
+
+function logoutUser() {
+  keycloak.logout()
+}
 </script>
 
 <template>
   <main>
+    <button @click="logoutUser">Se déconnecter</button>
     <TheWelcome />
   </main>
 </template>
