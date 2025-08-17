4e69df82a30dc5fdf0032ee9631e682c6f64cd20
Marcompiler <marcompiler@outlook.com>
2025-08-17 09:11:02 +0200

    NonNull handled in backend (_VSCodium warning_)

--- backend/app/src/main/java/com/marcompiler/backend/config/security/CorsConfig.java
+++ backend/app/src/main/java/com/marcompiler/backend/config/security/CorsConfig.java
@@ -3,6 +3,7 @@ package com.marcompiler.backend.config.security;
 import io.micrometer.common.lang.NonNullApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.NonNull;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -14,7 +15,7 @@ public class CorsConfig {
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
-            public void addCorsMappings(CorsRegistry registry) {
+            public void addCorsMappings(@NonNull CorsRegistry registry) {
                 registry.addMapping("/api/**")
                         .allowedOrigins("http://localhost:5173")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
