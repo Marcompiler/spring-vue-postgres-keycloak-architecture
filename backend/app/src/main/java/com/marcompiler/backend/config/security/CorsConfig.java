8ba78410a93d3c58bd122237e01c82d71b3cf730
Marcompiler <marcompiler@outlook.com>
2026-03-01 17:43:29 +0100

    Spring Boot upgraded to 4.0.3 + Corresponding adjustments in backend
    Migration guide : <https://github.com/spring-projects/spring-boot/wiki/Spring-Boot-4.0-Migration-Guide>

--- backend/app/src/main/java/com/marcompiler/backend/config/security/CorsConfig.java
+++ backend/app/src/main/java/com/marcompiler/backend/config/security/CorsConfig.java
@@ -1,13 +1,10 @@
 package com.marcompiler.backend.config.security;
 
-import io.micrometer.common.lang.NonNullApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.lang.NonNull;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@NonNullApi
 @Configuration
 public class CorsConfig {
 
@@ -15,7 +12,7 @@ public class CorsConfig {
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
-            public void addCorsMappings(@NonNull CorsRegistry registry) {
+            public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/**")
                         .allowedOrigins("http://localhost:5173", "http://localhost:4173", "http://localhost:8082")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
