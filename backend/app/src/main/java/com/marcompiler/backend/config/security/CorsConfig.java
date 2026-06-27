1760225cf0111c2e3cb518e3b5a867cfba285b0f
Marcompiler <marcompiler@outlook.com>
2026-06-27 10:11:47 +0200

    Removed unused origin/redirect_uri (:4173)

--- backend/app/src/main/java/com/marcompiler/backend/config/security/CorsConfig.java
+++ backend/app/src/main/java/com/marcompiler/backend/config/security/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/**")
-                        .allowedOrigins("http://localhost:5173", "http://localhost:4173", "http://localhost:8082")
+                        .allowedOrigins("http://localhost:5173", "http://localhost:8082")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("Authorization", "Content-Type");
             }
